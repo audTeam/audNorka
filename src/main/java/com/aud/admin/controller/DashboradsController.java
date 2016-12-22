88d334587284126fbec6af6382831cc3ca484243
Yuan Lunchuan <799652013@qq.com>
2016-12-22 09:45:21 +0800

    1, Add admin controller.

---
+++ src/main/java/com/aud/admin/controller/DashboradsController.java
@@ -0,0 +1,14 @@
+package com.aud.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/dashborad")
+public class DashboradsController {
+
+	@RequestMapping("")
+	public String show(){
+		return "admin/dashborad/show";
+	}
+}
