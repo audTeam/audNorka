14db912d74bb7f54189ba528538480b78b660440
Yuan Lunchuan <799652013@qq.com>
2016-12-23 08:44:37 +0800

    1, update dashborads.

--- src/main/java/com/aud/admin/controller/DashboradsController.java
+++ src/main/java/com/aud/admin/controller/DashboradsController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/dashborad")
 public class DashboradsController {
-
 	@RequestMapping("")
 	public String show(){
 		return "admin/dashborad/show";
