97bc7d69b205cb6552d8d4fc8c17121eca4ad49e
Yuan Lunchuan <799652013@qq.com>
2017-02-27 11:59:18 +0800

    1, ehcache.

---
+++ src/main/java/com/aud/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.aud.service;
+
+import java.util.List;
+
+import com.aud.pojo.User;
+
+public interface IUserService {
+	List<User> all();
+}
---
+++ src/main/java/com/aud/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.aud.service.impl;
+
+import java.util.List;
+
+import com.aud.pojo.User;
+import com.aud.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+
+	@Override
+	public List<User> all() {
+		return null;
+	}
+
+}
